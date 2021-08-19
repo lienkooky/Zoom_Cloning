e9205674cf094e3fbd9aa9e0865454618c385dde
GEONYEONG SON <thsrjs5@gmail.com>
2021-08-19 20:10:46 +0900

    hu

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,15 @@
-alert('H!');
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener('open', () => {
+  console.log('Conneted to Server ✅');
+});
+socket.addEventListener('message', (message) => {
+  console.log('just got this: ', message.data);
+});
+socket.addEventListener('close', () => {
+  console.log('Connected from Server🦊');
+});
+
+setTimeout(() => {
+  socket.send('hello from the browser!');
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,11 +1,28 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
+
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
+
+wss.on('connection', (socket) => {
+  console.log('Conneted to Browser ✅');
+  socket.on('close', () => {
+    console.log('Disconneted from th Browser 🦊');
+  });
+  socket.on('message', (message) => {
+    console.log(message);
+  });
+  socket.send('hello!!!');
+});
 
 const handelListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handelListen);
+server.listen(3000, handelListen);
