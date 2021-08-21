3a23769106f957b12d1b9ea564923bc3e18582d5
GEONYEONG SON <thsrjs5@gmail.com>
2021-08-21 19:26:19 +0900

    amazing

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,6 +4,7 @@ const myFace = document.getElementById('myFace');
 const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
 const camerasSelect = document.getElementById('cameras');
+
 let myStream;
 let muted = false;
 let cameraOff = false;
@@ -12,10 +13,15 @@ async function getCamears() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === 'videoinput');
+    const currentCamera = myStream.getVideoTracks()[0];
+    // console.log(cameras);
     cameras.forEach((camera) => {
       const option = document.createElement('option');
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
       camerasSelect.appendChild(option);
     });
   } catch (err) {
@@ -23,14 +29,23 @@ async function getCamears() {
   }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: 'user' },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    );
     myFace.srcObject = myStream;
-    await getCamears();
+    if (!deviceId) {
+      await getCamears();
+    }
   } catch (err) {
     console.log(err);
   }
@@ -62,8 +77,13 @@ function handleCameraClick() {
   }
 }
 
+async function handleCameraChange() {
+  await getMedia(camerasSelect);
+}
+
 muteBtn.addEventListener('click', handleMuteClick);
 cameraBtn.addEventListener('click', handleCameraClick);
+camerasSelect.addEventListener('input', handleCameraChange);
 
 /* ---------- socket.io를 이용해 메세지 보내는 방법 -------------------- */
 
