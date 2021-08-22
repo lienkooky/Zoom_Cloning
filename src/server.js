fc8efba70ac52ceebaa6ec733bb8fb6b1577c693
GEONYEONG SON <thsrjs5@gmail.com>
2021-08-22 19:25:47 +0900

    Peer to Peer

--- src/server.js
+++ src/server.js
@@ -15,8 +15,9 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on('connection', (socket) => {
-  socket.on('join_room', (roomName) => {
+  socket.on('join_room', (roomName, done) => {
     socket.join(roomName);
+    done();
     socket.to(roomName).emit('welcome');
   });
   socket.on('offer', (offer, roomName) => {
